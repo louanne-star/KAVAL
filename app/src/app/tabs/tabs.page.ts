836332ba628448b31eac2d2892d32ccf88ea0e51
Grosjean Lou-Anne <louannegjean@gmail.com>
2026-06-25 14:40:43 +1100

    modif css bleu

--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mapOutline, listOutline, informationCircleOutline } from 'ionicons/icons';
 
@@ -8,10 +11,32 @@ import { mapOutline, listOutline, informationCircleOutline } from 'ionicons/icon
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel]
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, CommonModule]
 })
 export class TabsPage {
-  constructor() {
+
+  ongletActif = signal<string>('carte');
+
+  readonly onglets = [
+    { id: 'carte',    icone: 'map-outline',                label: 'Carte'    },
+    { id: 'parcours', icone: 'list-outline',               label: 'Parcours' },
+    { id: 'apropos',  icone: 'information-circle-outline', label: 'À propos' },
+  ];
+
+  constructor(private router: Router) {
     addIcons({ mapOutline, listOutline, informationCircleOutline });
+
+    router.events
+      .pipe(filter(e => e instanceof NavigationEnd))
+      .subscribe((e: any) => {
+        const url: string = e.urlAfterRedirects ?? '';
+        if      (url.includes('/parcours')) this.ongletActif.set('parcours');
+        else if (url.includes('/apropos'))  this.ongletActif.set('apropos');
+        else                                this.ongletActif.set('carte');
+      });
+  }
+
+  navTo(tab: string) {
+    this.router.navigate([`/tabs/${tab}`]);
   }
 }
